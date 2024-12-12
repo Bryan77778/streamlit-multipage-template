7655600471f58d31985d9172c36a6c80f8ce10e5
Bryan77778 <s1143018@gm.ncue.edu.tw>
2024-12-12 20:35:02 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -18,19 +18,19 @@ st.title("Marker Cluster")
 with st.expander("See source code"):
     with st.echo():
 
-        # 建立地圖
-        m = leafmap.Map(center=[23.5, 121], zoom=7)
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
+        regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
 
-        # 新的資料路徑
-        water_quality_stations_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E6%B5%B7%E5%9F%9F%E6%B0%B4%E8%B3%AA%E6%B8%AC%E7%AB%99.geojson"
-        fishing_spots_url = "https://github.com/Bryan77778/11-27/raw/refs/heads/main/%E5%85%A8%E5%8F%B0%E9%96%8B%E6%94%BE%E9%87%A3%E9%BB%9E%E4%BD%8D%E7%BD%AE%20(1).geojson"
-
-        # 添加地理數據
-        m.add_geojson(water_quality_stations_url, layer_name='Water Quality Stations')
-        m.add_geojson(fishing_spots_url, layer_name='Fishing Spots')
-
-        # 如果需要對兩個 GeoJSON 文件中的點進行樣式設定：
-        # m.add_points_from_xy(), 顏色、圖示等可依需求設定。
-
-# 將地圖顯示於 Streamlit
+        m.add_geojson(regions, layer_name='US Regions')
+        m.add_points_from_xy(
+            cities,
+            x="longitude",
+            y="latitude",
+            color_column='region',
+            icon_names=['gear', 'map', 'leaf', 'globe'],
+            spin=True,
+            add_legend=True,
+        )
+        
 m.to_streamlit(height=700)
